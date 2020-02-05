1d7bd924e991a27f9a0bc325d11f4cb63431c883
Jonathan McGaha <jonathan.mcgaha@the-refinery.io>
2020-02-05 12:20:56 -0500

    Remove tailwind grid plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,45 @@
 module.exports = {
 	theme: {
 		container: {
-			center: true,
+			center: true
 		},
 		extend: {
 			colors: {
-				black: '#000',
+				black: "#000",
 				blue: {
-					300: '#3e86ff',
-					500: '#182ad5',
-					700: '#0a2342',
+					300: "#3e86ff",
+					500: "#182ad5",
+					700: "#0a2342"
 				},
 				gray: {
-					100: '#949699',
-					300: '#7c098b',
-					500: '#6d6d6d',
-					700: '#313747',
-					900: '#2a2d34',
+					100: "#949699",
+					300: "#7c098b",
+					500: "#6d6d6d",
+					700: "#313747",
+					900: "#2a2d34"
 				}
 			},
 			fontFamily: {
 				body: [
-					'Nunito Sans',
-					'-apple-system',
-					'BlinkMacSystemFont',
+					"Nunito Sans",
+					"-apple-system",
+					"BlinkMacSystemFont",
 					'"Segoe UI"',
-					'Roboto',
+					"Roboto",
 					'"Helvetica Neue"',
-					'Arial',
+					"Arial",
 					'"Noto Sans"',
-					'sans-serif',
+					"sans-serif",
 					'"Apple Color Emoji"',
 					'"Segoe UI Emoji"',
 					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"',
+					'"Noto Color Emoji"'
 				]
-			},
+			}
 		}
 	},
 	variants: {
-		display: ['responsive', 'hover', 'focus'],
+		display: ["responsive", "hover", "focus"]
 	},
-	plugins: [
-		require('@tailwindcss/custom-forms'),
-		require('tailwindcss-grid')({
-			grids: [2, 3, 4, 5, 6, 8, 10, 12],
-			gaps: {
-				0: '0',
-				4: '1rem',
-				8: '2rem',
-				'4-x': '1rem',
-				'4-y': '1rem',
-			},
-			autoMinWidths: {
-				'16': '4rem',
-				'24': '6rem',
-				'300px': '300px',
-			},
-			variants: ['responsive'],
-		}),
-	],
-}
+	plugins: [require("@tailwindcss/custom-forms")]
+};
