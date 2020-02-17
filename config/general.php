35a71ca55143953074353b5ba473554a70e51887
Jonathan McGaha <jonathan.mcgaha@the-refinery.io>
2020-02-17 00:02:25 -0500

    Simplify and Upgrade mix

--- config/general.php
+++ config/general.php
@@ -12,7 +12,7 @@ return [
   // Global settings
   '*' => [
     // Default Week Start Day (0 = Sunday, 1 = Monday...)
-    'defaultWeekStartDay' => 1,
+    'defaultWeekStartDay' => 0,
 
     // Whether generated URLs should omit "index.php"
     'omitScriptNameInUrls' => true,
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,25 +1,21 @@
-require('dotenv').config();
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-const glob = require('glob-all');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
-const LaravelMixFileHashVersioning = require('laravel-mix-file-hash-versioning');
+require("dotenv").config();
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+require("laravel-mix-purgecss");
 
-// Custom PurgeCSS extractor for Tailwind that allows special characters in class names.
-// https://github.com/FullHuman/purgecss#extractor
-class TailwindExtractor {
-	static extract(content) {
-		return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-	}
-}
+mix.js(["assets/javascript/app.js"], "public/assets/app.js").extract();
 
-mix.js(['assets/javascript/app.js'], 'public/assets/app.js').extract();
-
-mix.sass('assets/sass/app.scss', 'public/assets/app.css')
-	.setPublicPath('public')
+mix
+	.sass("assets/sass/app.scss", "public/assets/app.css")
+	.setPublicPath("public")
 	.options({
 		processCssUrls: false,
-		postCss: [tailwindcss('./tailwind.config.js')],
+		postCss: [tailwindcss("./tailwind.config.js")]
+	})
+	.purgeCss({
+		enabled: mix.inProduction(),
+		content: ["./templates/**/*.twig", "./assets/javascript/**/*.js"],
+		extensions: ["html", "js", "php", "twig", "vue"]
 	});
 
 // Run BrowserSync Locally, off by default
@@ -32,34 +28,8 @@ mix.sass('assets/sass/app.scss', 'public/assets/app.css')
 // 	});
 // }
 
-mix.webpackConfig({
-	plugins: [
-		new PurgecssPlugin({
-
-			// Specify the locations of any files you want to scan for class names.
-			paths: glob.sync([
-				path.join(__dirname, 'assets/javascript/*.js'),
-				path.join(__dirname, 'templates/**/*.twig'),
-			]),
-			extractors: [
-				{
-					extractor: TailwindExtractor,
-
-					// Specify the file extensions to include when scanning for class names.
-					extensions: ['html', 'js', 'twig'],
-				},
-			],
-		})
-	],
-});
-
 // Only run in Production
 if (mix.inProduction()) {
-	mix.webpackConfig({
-		plugins: [
-			// Fingerprints compiled assets with a hashed filename instead of query strings
-			// example: app.abc123.css
-			new LaravelMixFileHashVersioning(['public/assets']),
-		],
-	});
+	require("laravel-mix-versionhash");
+	mix.versionHash();
 }
